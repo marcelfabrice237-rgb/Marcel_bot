d64af55e2a84fae746b24fb4fb13cf1f1297e251
marcelfabrice237-rgb <marcelfabrice237@gmail.com>
2026-09-05 17:06:12 +0100

    Update react.js

--- utils/react.js
+++ utils/react.js
@@ -10,7 +10,7 @@ export default async function react(client, message){
 
         {
             react: {
-                text: '🎯',
+                text: '⚡',
 
                 key: message.key
             }
@@ -24,7 +24,7 @@ export default async function react(client, message){
 
         {
             react: {
-                text: '⚡',
+                text: '✅',
 
                 key: message.key
             }
