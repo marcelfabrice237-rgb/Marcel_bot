3ed0cb9bb366341f2e498964412117759213f395
marcelfabrice237-rgb <marcelfabrice237@gmail.com>
2026-09-05 16:58:37 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -40,7 +40,7 @@ export async function play(message, client) {
         
         await client.sendMessage(remoteJid, {
             image: { url: videoData.thumbnail },
-            caption: `🎵 *${videoData.title}*\n⏱️ ${videoData.duration || 'Inconnu'}\n👁️ ${videoData.views || 'Inconnu'} vues\n\n© Digital Crew 243`,
+            caption: `🎵 *${videoData.title}*\n⏱️ ${videoData.duration || 'Inconnu'}\n👁️ ${videoData.views || 'Inconnu'} vues\n\n© ꧁𓆩༒☥Kevin De Bruyne☥༒𓆪꧂`,
             quoted: message
         })
 
