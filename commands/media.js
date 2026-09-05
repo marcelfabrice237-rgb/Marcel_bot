14af4f9d2c69d5f7e88dbe6d6c7e336d737428e2
marcelfabrice237-rgb <marcelfabrice237@gmail.com>
2026-09-05 12:14:59 +0100

    Update media.js

--- commands/media.js
+++ commands/media.js
@@ -20,7 +20,7 @@ export async function photo(client, message) {
 
         await client.sendMessage(message.key.remoteJid, {
             image: fs.readFileSync(filename),
-            caption: '✨ Digital Crew 243'
+            caption: '✨ ꧁𓆩༒☥Kevin De Bruyne☥༒𓆪꧂'
         })
 
         fs.unlinkSync(filename)
@@ -40,7 +40,7 @@ export async function tomp3(client, message) {
         
         if (!target) {
             return await client.sendMessage(message.key.remoteJid, {
-                text: '🎵 *Digital Crew 243*\n\nRépondez à une vidéo pour extraire l\'audio.\n\nUsage: .toaudio (réponse à une vidéo)'
+                text: '🎵 *꧁𓆩༒☥Kevin De Bruyne☥༒𓆪꧂*\n\nRépondez à une vidéo pour extraire l\'audio.\n\nUsage: .toaudio (réponse à une vidéo)'
             })
         }
 
